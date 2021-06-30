406a1f0d3e3c3af03235733d1c2d4818877d5eb3
Ronald van Manen <rvanmanen@gmail.com>
2021-06-30 15:53:24 +0200

    Added binaries of FFmpeg v4.4 x64 to packages/ folder (downloaded from https://www.gyan.dev/ffmpeg/).

---
+++ packages/ffmpeg.runtime.win-x64/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:78f612e17c9a6858fcdc7a74a2145943381dd0cf93606d7b9268dba386c672a4
+size 1892
---
+++ packages/ffmpeg.runtime.win-x64/include/libpostproc/postprocess.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec163d6a1de0e9fb8083e01a1ad760514a9948cdbe9a9289b5525ba0aaaca9a4
+size 2936
