f6ab050db1aa1e2f5f8dc0bf24d9790c1be1719b
Ronald van Manen <rvanmanen@gmail.com>
2022-12-22 13:05:45 +0100

    Use `dotnet list` to determine the location of the referenced FFmpeg NuGet package.

--- generation/avfilter/generate.ps1
+++ generation/avfilter/generate.ps1
@@ -1,7 +1,26 @@
-$temporaryFile = "settings.rsp.tmp"
+try {
+  $PackageList = (dotnet list ..\..\sources\FFmpegSharp.Interop\FFmpegSharp.Interop.csproj package)
+  $PackageName = "FFmpeg"
+  $Package = $PackageList | Select-String -Pattern "^ +> $PackageName +"
+  $PackageVersionFound = $Package -match "^ +> $PackageName +(?<requestedVersion>[^ ]+) +(?<resolvedVersion>[^ ]+)"
+  if (-not $PackageVersionFound)
+  {
+    throw "$PackageName version not found"
+  }
 
-$settings = Get-Content -Path .\settings.rsp -Raw
-$settings = $settings -replace '\$HOME', $HOME
-Set-Content -Path $temporaryFile -Value $settings
+  $PackageVersion = $Matches.resolvedVersion
+  $PackageDirectory = "$HOME\.nuget\packages\$PackageName\$PackageVersion"
 
-& dotnet tool run ClangSharpPInvokeGenerator "@$temporaryFile"
+  $Settings = Get-Content -Path .\settings.rsp -Raw
+  $Settings = $Settings -replace '\$PackageDirectory', $PackageDirectory
+  $TemporarySettingsFile = "settings.rsp.tmp"
+  Set-Content -Path $TemporarySettingsFile -Value $Settings
+
+  & dotnet tool run ClangSharpPInvokeGenerator "@$TemporarySettingsFile"
+}
+catch {
+  Write-Host -Object $_
+  Write-Host -Object $_.Exception
+  Write-Host -Object $_.ScriptStackTrace
+  exit 1
+}
