c27bf1e518e59266243ecf62599137efb8f794d2
Ronald van Manen <rvanmanen@gmail.com>
2023-02-21 17:05:49 +0100

    Add command-line option --version for showing version.

--- tools/FFplaySharp/Program.cs
+++ tools/FFplaySharp/Program.cs
@@ -13,13 +13,15 @@
 // You should have received a copy of the GNU General Public License
 // along with FFmpegSharp.  If not, see <https://www.gnu.org/licenses/>.
 
+using System;
 using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
+using FFmpegSharp;
 
 namespace FFplaySharp
 {
-    internal sealed class Program
+    internal static class Program
     {
         static int Main(string[] args)
         {
@@ -36,11 +38,136 @@ namespace FFplaySharp
                 .UseTypoCorrections()
                 .UseParseErrorReporting()
                 .UseExceptionHandler()
-                .CancelOnProcessTermination();
+                .CancelOnProcessTermination()
+                .UseShowVersionOption();
 
             var commandLineParser = commandLineBuilder.Build();
 
             return commandLineParser.Invoke(args);
         }
+
+        private static CommandLineBuilder UseShowVersionOption(this CommandLineBuilder builder)
+        {
+            return builder.AddGlobalOption("--version", "Show version", ShowVersion);
+        }
+
+        private static void ShowVersion()
+        {
+            PrintBuildConfiguration();
+            PrintLibraryVersion("avutil");
+            PrintLibraryVersion("avcodec");
+            PrintLibraryVersion("avformat");
+            PrintLibraryVersion("avdevice");
+            PrintLibraryVersion("avfilter");
+            PrintLibraryVersion("swresample");
+            PrintLibraryVersion("swscale");
+        }
+
+        private static void PrintBuildConfiguration()
+        {
+            Console.WriteLine($"configuration: {AVUtil.BuildConfiguration}");
+        }
+
+        private static void PrintLibraryVersion(string libraryName)
+        {
+            var intVersion = GetIntVersion(libraryName);
+            Console.WriteLine("lib{0,-14} {1,2}.{2,3}.{3,3} / {4,2}.{5,3}.{6,3}",
+                libraryName,
+                GetMajorVersion(libraryName),
+                GetMinorVersion(libraryName),
+                GetMicroVersion(libraryName),
+                GetMajorVersion(intVersion),
+                GetMinorVersion(intVersion),
+                GetMicroVersion(intVersion));
+        }
+
+        private static uint GetIntVersion(string libraryName)
+        {
+            var type = typeof(FFmpegSharp.Interop.FFmpeg);
+            var method = type.GetMethod($"{libraryName.ToLower()}_version");
+            if (method == null)
+            {
+                return 0;
+            }
+            var result = method.Invoke(null, null);
+            if (result == null)
+            {
+                return 0;
+            }
+            return (uint)result;
+        }
+
+        private static int GetMajorVersion(string libraryName)
+        {
+            var type = typeof(FFmpegSharp.Interop.FFmpeg);
+            var field = type.GetField($"LIB{libraryName.ToUpper()}_VERSION_MAJOR");
+            if (field == null)
+            {
+                return 0;
+            }
+            var value = field.GetValue(null);
+            if (value == null)
+            {
+                return 0;
+            }
+            return (int)value;
+        }
+
+        private static int GetMinorVersion(string libraryName)
+        {
+            var type = typeof(FFmpegSharp.Interop.FFmpeg);
+            var field = type.GetField($"LIB{libraryName.ToUpper()}_VERSION_MINOR");
+            if (field == null)
+            {
+                return 0;
+            }
+            var value = field.GetValue(null);
+            if (value == null)
+            {
+                return 0;
+            }
+            return (int)value;
+        }
+
+        private static int GetMicroVersion(string libraryName)
+        {
+            var type = typeof(FFmpegSharp.Interop.FFmpeg);
+            var field = type.GetField($"LIB{libraryName.ToUpper()}_VERSION_MICRO");
+            if (field == null)
+            {
+                return 0;
+            }
+            var value = field.GetValue(null);
+            if (value == null)
+            {
+                return 0;
+            }
+            return (int)value;
+        }
+
+        private static uint GetMajorVersion(uint a) => (a) >> 16;
+
+        private static uint GetMinorVersion(uint a) => (((a) & 0x00FF00) >> 8);
+
+        private static uint GetMicroVersion(uint a) => ((a) & 0xFF);
+
+        private static CommandLineBuilder AddGlobalOption(this CommandLineBuilder builder, string alias, string description, Action callback)
+        {
+            var option = new Option<bool>(alias, description);
+            builder.Command.AddGlobalOption(option);
+            builder.AddMiddleware(async (context, next) =>
+            {
+                if (null != context.ParseResult.FindResultFor(option))
+                {
+                    callback();
+                }
+                else
+                {
+                    await next(context);
+                }
+            });
+            return builder;
+        }
+
     }
 }
