227573ea1a515c4273e71968f269eb212adb4b20
Ronald van Manen <rvanmanen@gmail.com>
2023-11-12 20:14:18 +0100

    Add sample for listing all available formats.

---
+++ samples/ListFormats/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FFmpegSharp;
+
+AVDevice.RegisterAll();
+
+Console.WriteLine("Formats:");
+Console.WriteLine(" D. = Demuxing supported");
+Console.WriteLine(" .E = Muxing supported");
+Console.WriteLine(" --");
+
+var inputFormats = AVInputFormat.All;
+var inputFormatNames = inputFormats.Select(inputFormat => inputFormat.Name);
+var inputFormatLookup = new Dictionary<string, AVInputFormat>();
+foreach (var inputFormat in inputFormats)
+{
+    inputFormatLookup.Add(inputFormat.Name, inputFormat);
+}
+
+var outputFormats = AVOutputFormat.All;
+var outputFormatNames = outputFormats.Select(outputFormat => outputFormat.Name);
+var outputFormatLookup = new Dictionary<string, AVOutputFormat>();
+foreach (var outputFormat in outputFormats)
+{
+    outputFormatLookup.Add(outputFormat.Name, outputFormat);
+}
+
+var formatNames = Enumerable.Union(inputFormatNames, outputFormatNames);
+foreach (var formatName in formatNames.OrderBy(e => e))
+{
+    var hasInputFormat = inputFormatLookup.TryGetValue(formatName, out var inputFormat);
+    var hasOutputFormat = inputFormatLookup.TryGetValue(formatName, out var outputFormat);
+    var formatLongName = inputFormat?.LongName ?? outputFormat?.LongName;
+
+    Console.WriteLine(" {0}{1} {2,-15} {3}",
+        hasInputFormat ? "D" : " ",
+        hasOutputFormat ? "E" : " ",
+        formatName,
+        formatLongName ?? " ");
+}
