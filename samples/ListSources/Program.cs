27420332e082111a08302506b94eb49cf70785e0
Ronald van Manen <rvanmanen@gmail.com>
2023-11-14 10:40:41 +0100

    Add sample project listing all available sources for a specific input device.

---
+++ samples/ListSources/Program.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2021-2023 Ronald van Manen
+//
+// This file is part of FFmpegSharp.
+//
+// FFmpegSharp is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// FFmpegSharp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with FFmpegSharp; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+using System;
+using System.Linq;
+using FFmpegSharp;
+
+AVDevice.RegisterAll();
+
+var inputDevices = AVDevice.InputDevices.Where(e => e.Name.Contains(args[0]));
+foreach (var inputDevice in inputDevices)
+{
+    Console.WriteLine("Auto-detected sources for {0}:", inputDevice.Name);
+
+    var inputSources = inputDevice.InputSources;
+    if (inputSources == null)
+    {
+        Console.WriteLine("Cannot list sources. Not implemented.");
+    }
+    else
+    {
+        for (var inputSourceIndex = 0; inputSourceIndex < inputSources.Count; ++inputSourceIndex)
+        {
+            Console.WriteLine("{0} {1} [{2}]",
+                inputSources.DefaultDeviceIndex == inputSourceIndex ? "*" : " ",
+                inputSources[inputSourceIndex].Name,
+                inputSources[inputSourceIndex].Description);
+        }
+    }
+}
